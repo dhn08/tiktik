e839e6a6d86d045b9993ea9034090b8d19bca809
Dhananjay <singhdhananjay.2001@gmail.com>
2022-07-24 23:17:32 +0530

    trying error boundary

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,31 @@ import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 function MyApp({ Component, pageProps }) {
   const [isSSR, setIsSSR] = useState(true);
   useEffect(() => {
@@ -10,19 +35,21 @@ function MyApp({ Component, pageProps }) {
   }, []);
   if (isSSR) return null;
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT}>
-      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
-        <Navbar />
-        <div className="flex gap-6 md:gap-20">
-          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-            <Sidebar />
-          </div>
-          <div className="mt-4 flex flex-col gap-10 overflow-y-auto h-[88vh] videos flex-1">
-            <Component {...pageProps} />
+    <ErrorBoundary>
+      <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT}>
+        <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+          <Navbar />
+          <div className="flex gap-6 md:gap-20">
+            <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+              <Sidebar />
+            </div>
+            <div className="mt-4 flex flex-col gap-10 overflow-y-auto h-[88vh] videos flex-1">
+              <Component {...pageProps} />
+            </div>
           </div>
         </div>
-      </div>
-    </GoogleOAuthProvider>
+      </GoogleOAuthProvider>
+    </ErrorBoundary>
   );
 }
 
