08f6b45cc3c9af8190cb93ceab2814ca9b9d266a
Dhananjay <singhdhananjay.2001@gmail.com>
2022-07-25 01:12:17 +0530

    https://tiktik-dhn.vercel.app removed

--- store/authStore.js
+++ store/authStore.js
@@ -4,12 +4,12 @@ import axios from "axios";
 
 const authStore = (set) => ({
   userProfile: null,
-  allUsers: [null],
+  allUsers: [],
   addUser: (user) => set({ userProfile: user }),
   removeUser: (user) => set({ userProfile: null }),
 
   fetchAllUsers: async () => {
-    const response = await axios.get("https://tiktik-dhn.vercel.app/api/users");
+    const response = await axios.get("/api/users");
     console.log("inside fetch all user :", response);
     set({ allUsers: response.data });
   },
