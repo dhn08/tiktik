9d84197af2c25605d2a67741679d43506452a3e2
Dhananjay <singhdhananjay.2001@gmail.com>
2022-07-25 00:22:10 +0530

    adjusting error boundary

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,9 @@ function MyApp({ Component, pageProps }) {
 
         <div className="flex gap-6 md:gap-20">
           <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-            <Sidebar />
+            <ErrorBoundary>
+              <Sidebar />
+            </ErrorBoundary>
           </div>
           <div className="mt-4 flex flex-col gap-10 overflow-y-auto h-[88vh] videos flex-1">
             <ErrorBoundary>
